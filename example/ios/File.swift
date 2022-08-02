9868e48996913dfc74fdd3880ef27569d6343ec9
Emre <emre@happytech.com>
2022-08-02 14:04:27 +0300

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  TvosSliderExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-tvos-slider': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
